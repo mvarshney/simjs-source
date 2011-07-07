3743f598ac4d786795b1ef204d52a2bd835cdfed
mvarshney <mvarshney@4c3720d7-0463-a108-9567-e158911378b1>
2011-07-07 05:17:21 +0000

    added Sim.Store

--- src/sim.js
+++ src/sim.js
@@ -115,6 +115,24 @@ Sim.prototype.addEntity = function (proto) {
 			return ro;
 		};
 		
+		proto.putStore = function (store, obj) {
+			ARG_CHECK(arguments, 2, 2, Sim.Store);
+			
+			var ro = new Sim.Request(this, this.sim.time(), 0);
+			ro.source = store;
+			store.put(obj, ro);
+			return ro;
+		};
+		
+		proto.getStore = function (store, filter) {
+			ARG_CHECK(arguments, 1, 2, Sim.Store, Function);
+			
+			var ro = new Sim.Request(this, this.sim.time(), 0);
+			ro.source = store;
+			store.get(filter, ro);
+			return ro;
+		};
+		
 		proto.send = function (message, delay, entities) {
 			ARG_CHECK(arguments, 2, 3);
 			
@@ -508,6 +526,172 @@ Sim.Buffer.prototype.putStats = function () {
 Sim.Buffer.prototype.getStats = function () {
 	return this.getQueue.stats;
 };
+
+
+/** Store
+ * 
+ */
+
+Sim.Store = function (name, capacity) {
+	ARG_CHECK(arguments, 2, 3);
+	
+	this.name = name;
+	this.capacity = capacity;
+	this.objects = [];
+	this.putQueue = new Sim.Queue();
+	this.getQueue = new Sim.Queue();
+};
+
+Sim.Store.prototype.current = function () {
+	return this.objects.length;
+};
+
+Sim.Store.prototype.size = function () {
+	return this.capacity;
+};
+
+Sim.Store.prototype.get = function (filter, ro) {
+	ARG_CHECK(arguments, 2, 2);
+	
+	if (this.getQueue.empty() && this.current() > 0) {
+		var found = false;
+		var obj;
+		// TODO: refactor this code out
+		// it is repeated in progressGetQueue
+		if (filter) {
+			for (var i = 0; i < this.objects.length; i++) {
+				obj = this.objects[i];
+				if (filter(obj)) {
+					found = true;
+					this.objects.splice(i, 1);
+					break;
+				}
+			}
+		} else {
+			obj = this.objects.shift();
+			found = true;
+		}
+		
+		if (found) {
+			this.available --;
+			
+			ro.msg = obj;
+			ro.deliverAt = ro.entity.time();
+			ro.entity.sim.queue.insert(ro);
+
+			this.getQueue.passby(ro.deliverAt);
+
+			this.progressPutQueue();
+
+			return;
+		}
+	}
+	
+	ro.filter = filter;
+	this.getQueue.push(ro, ro.entity.time());
+};
+
+Sim.Store.prototype.put = function (obj, ro) {
+	ARG_CHECK(arguments, 2, 2);
+
+	if (this.putQueue.empty() && this.current() < this.capacity) {
+		this.available ++;
+		
+		ro.deliverAt = ro.entity.time();
+		ro.entity.sim.queue.insert(ro);
+		
+		this.putQueue.passby(ro.deliverAt);
+		this.objects.push(obj);
+		
+		this.progressGetQueue();
+		
+		return;
+	}
+
+	ro.obj = obj;
+	this.putQueue.push(ro, ro.entity.time());
+};
+
+Sim.Store.prototype.progressGetQueue = function () {
+	var ro;
+	while (ro = this.getQueue.top()) {
+		// if obj is cancelled.. remove it.
+		if (ro.cancelled) {
+			this.getQueue.shift(ro.entity.time());
+			continue;
+		} 
+		
+		// see if this request can be satisfied
+		if (this.current() > 0) {
+			var filter = ro.filter;
+			var found = false;
+			var obj;
+			
+			if (filter) {
+				for (var i = 0; i < this.objects.length; i++) {
+					obj = this.objects[i];
+					if (filter(obj)) {
+						found = true;
+						this.objects.splice(i, 1);
+						break;
+					}
+				}
+			} else {
+				obj = this.objects.shift();
+				found = true;
+			}
+			
+			if (found) {
+				// remove it..
+				this.getQueue.shift(ro.entity.time());
+				this.available --;
+				
+				ro.msg = obj;
+				ro.deliverAt = ro.entity.time();
+				ro.entity.sim.queue.insert(ro);
+			} else {
+				break;
+			}
+		
+		} else {
+			// this request cannot be satisfied
+			break;
+		}
+	}
+};
+
+Sim.Store.prototype.progressPutQueue = function () {
+	var ro;
+	while (ro = this.putQueue.top()) {
+		// if obj is cancelled.. remove it.
+		if (ro.cancelled) {
+			this.putQueue.shift(ro.entity.time());
+			continue;
+		} 
+		
+		// see if this request can be satisfied
+		if (this.current() < this.capacity) {
+			// remove it..
+			this.putQueue.shift(ro.entity.time());
+			this.available ++;
+			this.objects.push(ro.obj);
+			ro.deliverAt = ro.entity.time();
+			ro.entity.sim.queue.insert(ro);
+		} else {
+			// this request cannot be satisfied
+			break;
+		}
+	}
+};
+
+Sim.Store.prototype.putStats = function () {
+	return this.putQueue.stats;
+};
+
+Sim.Store.prototype.getStats = function () {
+	return this.getQueue.stats;
+};
+
 
 /** Event
  * 
