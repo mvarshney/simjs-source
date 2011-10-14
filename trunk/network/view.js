19bfee16f01c7a7b632c7a383c595a760142dae9
mvarshney <mvarshney@4c3720d7-0463-a108-9567-e158911378b1>
2011-10-14 17:37:23 +0000

    added

---
+++ trunk/network/view.js
@@ -0,0 +1,45 @@
+var View = {
+	reset: function () {
+		
+	},
+	
+	clear: function() {
+		
+	},
+	
+	addHost: function(name, type, label, x, y, yindex){
+		
+	},
+	
+	connect: function(from, to){
+		
+	},
+	
+	addInfo: function(time, at, msg, duration){
+		
+	},
+	
+	addPacket: function(time, from, to, msg){
+		
+	},
+	
+	addPacketDrop: function(time, at, msg){
+		
+	},
+	
+	addTable: function(host, name, title, nrows, ncols, x, y){
+		
+	},
+	
+	setTableVal: function(node, name, row, col, val){
+		
+	},
+	
+	addDiv: function(host, div, x, y){
+		
+	},
+	
+	step: function(){
+		
+	},
+};
