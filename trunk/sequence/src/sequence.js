1b40264afe4de75a58c8de588e510db6d2d4b352
mvarshney <mvarshney@4c3720d7-0463-a108-9567-e158911378b1>
2011-11-05 22:42:26 +0000

    new parser

--- trunk/sequence/src/sequence.js
+++ trunk/sequence/src/sequence.js
@@ -2,9 +2,156 @@ var Icons = {
 	host: {img: "images/host.png", w: 68, h: 48},
 	cloud: {img: "images/cloud_orange.png", w: 89, h: 47},
 	server: {img: "images/server.png", w: 64, h: 74},
+	webserver: {img: "images/webserver.png", w: 38, h: 38},
 	trash: {img: "images/trash.png", w:24, h:24},
 	cookie: {img: "images/cookie.png", w: 48, h: 48}
 };
+
+Array.prototype.trim = function () {
+	return $.map(this, $.trim);
+}
+
+function readOptions(lines, num) {
+	var options = {}, len = lines.length, pairs, npairs, i, keyval;
+	num ++;
+	while (num < len && lines[num][0] === '-') {
+		pairs = lines[num].substr(lines[num].indexOf('-') + 1).split(',').trim();
+		for (i = pairs.length - 1; i>=0; i--) {
+			keyval = pairs[i].split('=').trim();
+			if (keyval[0].length === 0) continue;
+			options[keyval[0]] = keyval[1];
+		}
+		
+		num++;
+	}
+	
+	return options;
+}
+
+function ParseText(str) {
+	var conf = {version: 1, objects: {}, panels: []},
+		cPanel = null,
+		panelId = 0,
+		lines = str.split('\n').trim(),
+		nlines = lines.length,
+		line, words, lineno, i, j;
+	
+	
+	for (lineno = 0; lineno < nlines; lineno++) {
+		line = lines[lineno];
+		if (line.length === 0) continue;
+		if (line[0] === '#') continue;
+		words = line.split(/\s+/).trim();
+		if (words[0][0] === '-') continue;
+
+		// Define command. 
+		if (words[0] === 'define') {
+			var obj = {type: [words[2], words[3], words[4]], 
+						scale: words[5], 
+						x: words[6], 
+						y: words[7], 
+						hidden: (words[8] === 'hidden')};
+			
+			if (cPanel) {
+				cPanel.events.push({type: 'define', name: words[1], obj: obj});
+
+			} else {
+				conf.objects[words[1]] = obj;
+			}
+			continue;
+		}
+		
+		if (words[0] === 'done') break;
+		
+		// panel command
+		if (words[0] === 'panel') {
+			cPanel = {name: 'panel_' + panelId ++,
+						events: [], 
+						parent: cPanel ? cPanel.name : "", 
+						msg: line.split('panel ')[1],
+						options: readOptions(lines, lineno)};
+			conf.panels.push(cPanel);
+			continue;
+		}
+		
+		if (words[0] === 'move') {
+			var objs = line.split(words[0])[1].split(',').trim();
+			for (i = objs.length - 1; i >= 0; i--) {
+				j = objs[i].split(/\s+/);
+				cPanel.events.push({type: 'move',
+									at: j[0],
+									x: j[1],
+									y: j[2],
+									options: {permanent: j[3] === 'permanent'}});
+			}
+				
+			continue;
+		}
+		
+		if (words[0] === 'show' || words[0] === 'hide') {
+			var objs = line.split(words[0])[1].split(',').trim();
+			for (i = objs.length - 1; i >= 0; i--) {
+				j = objs[i].split(/\s+/);
+				cPanel.events.push({type: words[0], at: j[0],
+						options: {permanent: j[1] === 'permanent'}});
+			}
+			
+			continue;
+		}
+		
+		//----- The basic elements
+		if (line.indexOf(':') === -1) {
+			console.log(line);
+			continue;
+		}
+		
+		j = line.split(':', 2);
+		var send = (j[0].indexOf(' to ') !== -1);
+		var from, to, ev = {x: 0, y: 0};
+		
+		if (send) {
+			ev.type = 'send';
+			from = j[0].split(' to ')[0];
+			to = j[0].split(' to ')[1];
+			to = to.split(',').trim();
+			if (to.length === 1) {
+				ev.to = to[0];
+			} else {
+				ev.to = to;
+			}
+		} else {
+			ev.type = 'say';
+			from = j[0];
+		}
+
+		from = from.split(',').trim();
+		if (from.length === 1) {
+			ev.at = from[0];
+		} else {
+			ev.at = from;
+		}
+		
+		ev.msg = $.trim(j[1]);
+		ev.msg = ev.msg.replace(/\\n/g, '\n');
+		ev.options = readOptions(lines, lineno);
+		
+		// HACK -- special cases
+		if (ev.options.angles) 
+			ev.options.angles = ev.options.angles.split(/\s+/).trim();
+		
+		j = $.trim(lines[lineno + 1]);
+		if (j && j[0] === '-') {
+			j = j.split(/\s+/);
+			ev.x = parseInt(j[1]); 
+			ev.y = parseInt(j[2]);
+		}
+		
+		cPanel.events.push(ev);
+	}
+	
+	console.log(JSON.stringify(conf, null, 4));
+	return conf;
+}
 
 function Parse(str) {
 
@@ -66,27 +213,6 @@ function Parse(str) {
 	return conf;
 }
 
-
-function model(define, panel, say, send, hide, show, move) {
-	define('A', 'host', 60, 20, 80);
-	define('B', 'host', 40, 80, 50);
-	
-	panel("title", "", {type: 'banner'});
-	panel("One fine morning...", "");
-	say(['A', 'B'], 'Hello  World', 30, 30, {angles: [[-90, 90], [0	, 180]], type: 'cloud'});
-	say('B', 'Me too', 70, 30, {type: 'ellipse'});
-	
-	panel('', '', {width: 70, clear: true, frame: false});
-	say(null, 'Something\ngreat\nwill\nhappen\nnext', 50, 50, {type: 'cloud', curvy: 20});
-
-	panel("second panel..");
-//	move('B', 80, 80)
-	send('A', 'B', 'message', 50, 20, {angles: [90, 180], curvy: 30});
-	send('B', 'A', 'message', 90, 20, {angles: [-90, -90], curvy: 40});
-	panel("title", "", {type: 'banner'});
-	panel();
-}
-
 function endPoint(rect, angle, gap) {
 	if (gap === undefined) gap = 0;
 	
@@ -100,14 +226,14 @@ function endPoint(rect, angle, gap) {
 	while (angle >= 360) angle -= 360;
 
 	if (angle >= 45 && angle < 135) {
-		return {x: a + w/2 - h / 2 / tan, y: b};
+		return {x: a + w/2 - h/2/tan, y: b};
 	}
 	if (angle >= 135 && angle < 225) {
 		return {x: a, y: b + h/2 - tan * w / 2};
 	}
 	
 	if (angle >= 225 && angle < 315) {
-		return {x: a + w/2 + h / 2 / tan, y: b + h};
+		return {x: a + w/2 + h/2/tan, y: b + h};
 	}
 	
 	return {x: a + w, y: b + h/2 + tan * w/2};
@@ -131,24 +257,66 @@ function controlPoint(point, angle, stretch) {
 }
 
 function handle_say(cs, pconf, pw, ph, ev) {
-	var tx, txbb, b, bb;
-	var type = 'none';
+	var tx, txbb, b, bb, path, g1, g2,
+		op = ev.options || {},
+		color = op.color || '#fff',
+		type = op.type || (ev.at ? 'balloon' : 'cloud'),
+		style = op.style || '_',
+		tailwidth = op.twidth === undefined ? 5 : parseInt(op.twidth),
+		tailangle = (op.tangle === undefined) ? 0 : parseInt(op.tangle),
+		tailcurvy = op.tcurvy === undefined ? 20 : parseInt(op.tcurvy);
+		
 	
-	if (ev.options) {
-		if (ev.options.type) type = ev.options.type;
-	}
-	
+	// Step 1: draw text
 	tx = cs.text(pconf.x + pw * ev.x / 100,
 					pconf.y + ph * ev.y / 100,
 					ev.msg)
 			.attr({'font-family': '"Comic Sans MS", cursive, sans-serif'});
 	txbb = tx.getBBox();
 	
+	// Step 2: draw tail
+	if (ev.at) {
+		var srcangle = -90, dstangle = 90;
+		
+		if (ev.at instanceof Array) {
+			path = [];
+			for (var i = ev.at.length - 1; i >= 0; i--) {
+				var angle = (op.angle === undefined) ? 90 : parseInt(op.angle);
+				from = {x: txbb.x + txbb.width/2, y: txbb.y + txbb.height/2};
+				var to = endPoint(pconf.objects[ev.at[i]].getBBox(), angle);
+				path.push(cs.path(['M', from.x-tailwidth, from.y, 
+					'L', to.x, to.y, from.x+tailwidth, from.y, 'Z'].join(','))
+					.attr({'stroke-width': 1, 'stroke-dasharray': style, fill: color}));
+				
+			}
+		} else {
+			var angle = (op.angle === undefined) ? 90 : parseInt(op.angle);
+			from = {x: txbb.x + txbb.width/2, y: txbb.y + txbb.height/2};
+			var to = endPoint(pconf.objects[ev.at].getBBox(), angle);
+			
+			if (tailangle) {
+				g1 = controlPoint({x:to.x, y: to.y}, tailangle, tailcurvy);
+				cs.circle(g1.x, g1.y, 2);
+				path = cs.path(['M', to.x, to.y,
+								'Q', g1.x, g1.y, from.x-tailwidth, from.y,
+								'M', to.x, to.y,
+								'Q', g1.x, g1.y, from.x+tailwidth, from.y,
+								].join(','));
+			} else {
+				path = cs.path(['M', from.x-tailwidth, from.y, 
+					'L', to.x, to.y, from.x+tailwidth, from.y, 'Z'].join(','));
+			}
+			path.attr({'stroke-width': 1, 'stroke-dasharray': style, fill: color});
+		}	
+	}
+	
+	// Step 3: Draw balloon
 	if (type === 'rect') {
 		b = cs.rect(txbb.x - 2,
 				txbb.y - 2,
 				txbb.width + 4,
-				txbb.height + 4).attr({fill: 'white', 'stroke-width': 0.5});
+				txbb.height + 4)
+		.attr({fill: color, 'stroke-width': 0.5, 'stroke-dasharray': style});
 		bb = b.getBBox();
 		
 	} else if (type === 'ellipse') {
@@ -156,48 +324,71 @@ function handle_say(cs, pconf, pw, ph, ev) {
 				txbb.x + txbb.width / 2, 
 				txbb.y + txbb.height / 2, 
 				txbb.width/2 + 10,
-				txbb.height).attr({fill: 'white','stroke-width': 0.5});
+				txbb.height)
+		.attr({fill: color,'stroke-width': 0.5, 'stroke-dasharray': style});
 		bb = b.getBBox();
 	} else if (type === 'cloud') {
-		var a = txbb.x, b = txbb.y, w = txbb.width, h = txbb.height, g = 10;
-		if (ev.options && ev.options.curvy) g = ev.options.curvy;
-		var path = cs.path(['M', a, b, 
+		var a = txbb.x, 
+			b = txbb.y, 
+			w = txbb.width + 2, 
+			h = txbb.height + 2, 
+			g = Math.max(15, w/6);
+		if (ev.options && ev.options.curvy) g = parseInt(ev.options.curvy);
+		b = cs.path(['M', a, b, 
 				'C', a, b-g, a+w, b-g, a+w, b,
 				'C', a+w+g, b, a+w+g, b+h, a+w, b+h,
 				'C', a+w, b+h+g, a, b+h+g, a, b+h,
 				'C', a-g, b+h, a-g, b, a, b,
-				'Z'].join(',')).attr({fill: 'white'});
-		b = path;
+				'Z'].join(','))
+		.attr({fill: color, 'stroke-dasharray': style});
+		bb = b.getBBox();
+	} else if (type === 'balloon') {
+		var a = txbb.x, b = txbb.y, w = txbb.width + 2, h = txbb.height + 2, g = 10;
+		g1 = Math.min(w/6, 15);
+		g2 = h/4;
+		var dd = ['M', a, b,
+				'C', a+g1, b-g1, a+w-g1, b-g1, a+w, b,
+				'C', a+w+g2, b+g2, a+w+g2, b+h-g2, a+w, b+h,
+				'C', a+w-g1, b+h+g1, a+g1, b+h+g1, a, b+h,
+				'C', a-g2, b+h-g2, a-g2, b+g2, a, b,
+				'Z'].join(',');
+				
+		b = cs.path(dd).attr({'stroke-dasharray': style, 'stroke-width': 1.5});
+		b = cs.path(dd).attr({fill: color, stroke: 'none'});
+		
+		
+//		b.attr({fill: color, 'stroke-dasharray': style});
+		
+		
 		bb = b.getBBox();
 	} else {
 		bb = txbb;
 	}
+	
+	// Step 4: bring text to front
+	if (path) {
+		if (path instanceof Array) $.each(path, function (i, p){p.toFront();});
+		else path.toFront();
+	}
+	if (b) b.toFront();
 	tx.toFront();
 
-	if (ev.at) {
-		var srcangle = -90, dstangle = 90;
-		
-		
-		if (ev.at instanceof Array) {
-			for (var z = 0; z < ev.at.length; z++) {
-				if (ev.options && ev.options.angles) 
-					srcangle = ev.options.angles[z][0];
-				if (ev.options && ev.options.angles) 
-					dstangle = ev.options.angles[z][1];
-				var from = endPoint(bb, srcangle);
-				var to = endPoint(pconf.objects[ev.at[z]].getBBox(), dstangle);
-				var line = ['M', from.x, ' ', from.y, 'L', to.x, ' ', to.y].join();
-				cs.path(line).attr({'stroke-dasharray': '-'});
-			}
-		} else {
-			if (ev.options && ev.options.angles) srcangle = ev.options.angles[0];
-			if (ev.options && ev.options.angles) dstangle = ev.options.angles[1];
-			var from = endPoint(bb, srcangle);
-			var to = endPoint(pconf.objects[ev.at].getBBox(), dstangle);
-			var line = ['M', from.x, ' ', from.y, 'L', to.x, ' ', to.y].join();
-			cs.path(line).attr({'stroke-dasharray': '-'});							
-		}
-		
+	
+	if (pconf.conf.design) {
+		tx.pconf = pconf;
+		tx.drag(
+			function (dx, dy) {
+				this.attr({x: this.ox + dx, y: this.oy + dy});
+			},
+			function () {
+				this.ox = this.attr('x');
+				this.oy = this.attr('y');
+			},
+			function () {
+				this.attr('title',
+					[ ((this.attr('x') - pconf.x) * 100 / pconf.width).toFixed(0),
+					((this.attr('y') - pconf.y) * 100 / pconf.height).toFixed(0)].join(', '));
+			});
 	}
 }
 
@@ -273,6 +464,23 @@ function handle_send(cs, pconf, pw, ph, ev) {
 		var from = endPoint(bb, srcangle, 5);
 		var line = ['M', from.x, ' ', from.y, 'L', mid.x, ' ', mid.y].join();
 		cs.path(line);
+		
+		if (pconf.conf.design) {
+			tx.pconf = pconf;
+			tx.drag(
+				function (dx, dy) {
+					this.attr({x: this.ox + dx, y: this.oy + dy});
+				},
+				function () {
+					this.ox = this.attr('x');
+					this.oy = this.attr('y');
+				},
+				function () {
+				this.attr('title',
+						[ ((this.attr('x') - pconf.x) * 100 / pconf.width).toFixed(0),
+						((this.attr('y') - pconf.y) * 100 / pconf.height).toFixed(0)].join(', '));
+				});
+		}
 	}
 }
 
@@ -289,13 +497,15 @@ function drawComics(conf) {
 	for (i = 0; i < npanels; i++) {
 		var pconf = conf.panels[i],
 			op = pconf.options || {},
-			pw = op.width || conf.panelw,
+			pw = op.width === undefined ? conf.panelw : parseInt(op.width),
 			ph = conf.panelh,
 			framed = op.frame !== undefined ? op.frame : true,
 			type = op.type !== undefined ? op.type : 'simple',
 			color = op.color || '#fff', // ''90-#8df-#fff',
 			cleared = op.clear;
-	
+		
+		pconf.conf = conf;
+
 		// Determine Panel boundary
 		if (type === 'banner') {
 	
@@ -307,6 +517,7 @@ function drawComics(conf) {
 			ph = 20;
 			cleared = true;
 		}
+
 	 	if (currx + conf.gap + pw > conf.width) {
 			currx = 0;
 			curry += ph + conf.gap;
@@ -350,7 +561,11 @@ function drawComics(conf) {
 		if (!cleared) {
 		for (name in conf.objects) {
 			obj = conf.objects[name];
-			icon = Icons[obj.type];
+			if (obj.type instanceof Array) {
+				icon = {img: obj.type[0], w: obj.type[1], h: obj.type[2]};
+			} else {
+				icon = Icons[obj.type];
+			}
 			if (!icon) throw "Object type " + obj.type + " is not supported";
 			pconf.objects[name] = 
 						cs.image(icon.img, 
@@ -369,8 +584,13 @@ function drawComics(conf) {
 			ev = pconf.events[j];
 			switch (ev.type) {
 				case 'move': {
-					pconf.objects[ev.at].attr({x: pconf.x + pw * ev.x / 100, 
-												y: pconf.y + ph * ev.y / 100});
+					var obj = pconf.objects[ev.at];
+					obj.attr({x: pconf.x + pw * ev.x / 100 - obj.attr('width') / 2, 
+							  y: pconf.y + ph * ev.y / 100 - obj.attr('height') / 2});
+					if (ev.options && ev.options.permanent) {
+						conf.objects[ev.at].x = ev.x;
+						conf.objects[ev.at].y = ev.y;
+					}
 					break;
 				}
 				case 'hide': {
@@ -409,53 +629,44 @@ function drawComics(conf) {
 	
 }
 
+function drawInput(canvas, width, height, input, design) {
+	var conf = ParseText(input);
 
-
-function drawInput(cm) {
-//	var input = $('#input').val();
-	var input = cm.getValue();
-	var conf;
-	try {
-		conf = Parse(input);
-	} catch (e) {
-		$('#error').html("Syntax error in input");
-		return;
-	}
-	$('#error').html('Successfully parsed');
-	conf.width = $('#page').width();
-	conf.height = $('#page').height();
+	conf.canvas = canvas;
+	conf.width = width;
+	conf.height = height;
 	conf.gap = 10;
-	conf.panelw = 180;
-	conf.panelh = 140;
-	conf.canvas = CANVAS;
+	conf.panelw = 210;
+	conf.panelh = 160;
+	
+	conf.design = design;
 
 	drawComics(conf);
 }
 
+/*
 var CANVAS;
 $(function () {
 	CANVAS = Raphael('page', $('#page').width(), $('#page').height());
 	
-	if (!false)	 {
-		var a = model.toString().split("\n");
-		a[0] = null;
-		a[a.length - 1] = null;
-		modelstr = a.join("\n");
-		var conf = Parse(crosssitescripting);
-
-		conf.width = $('#page').width();
-		conf.height = $('#page').height();
-		conf.gap = 10;
-		conf.panelw = 180;
-		conf.panelh = 140;
-		conf.canvas = CANVAS;
-
-		drawComics(conf);
-	}
+	$('.comic_links').click(function () {
+		var link = $(this).attr('href');
+		var func = {'#xss1': crosssitescripting, 
+					'#xss2': crosssitescripting2,
+					'#dhcp': dhcpbasics,
+					'#hdfs': hdfs};
+		drawInput(func[link]);
+		return false;
+	});
 	
+	drawInput(test, true);
+
+	
+//	console.log($('#page').html());
 	$('#input').blur(drawInput);
 	$('#execute').click(drawInput);
 });
+*/
 
 Raphael.fn.arrow = function(x1, y1, x2, y2, size) {
   var angle = Raphael.angle(x1, y1, x2, y2);
